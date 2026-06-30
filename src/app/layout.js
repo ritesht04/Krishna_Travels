e53bd0a6857ab75f6073e5534866144590c10c1f
namannb07 <namannb45@gmail.com>
2026-07-01 01:25:02 +0530

    Added keywords for seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,10 @@ export const metadata = {
   description:
     "Krishna Tour & Travels offers trusted taxi services in Ujjain & Indore. Book cabs for Omkareshwar, Bhopal, Baglamukhi, Mandu, Maheshwar, local sightseeing, airport pickup-drop & outstation travel.",
   keywords:
-    "taxi service in ujjain, cab service ujjain, ujjain to omkareshwar taxi, ujjain to indore cab, ujjain to bhopal taxi, indore airport taxi, ujjain outstation cab",
+    "taxi service in ujjain, cab service ujjain, ujjain to omkareshwar taxi, ujjain to indore cab, ujjain to bhopal taxi, indore airport taxi, ujjain outstation cab, Ujjain, Ujjain tours, Ujjain travels, Ujjain places, Mahakaleshwar, Harsiddhi, Omkareshwar, Chintaman Ganesh",
+  verification: {
+    google: "zegv5UgWN477wc-eWFmIAK2RS7CVMPY8QLz7q9NR1do",
+  },
 };
 
 export const viewport = {
